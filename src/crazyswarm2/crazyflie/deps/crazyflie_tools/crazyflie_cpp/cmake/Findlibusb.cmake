e110058a6545df7b2721a0b645278ef351cea1a6
jc-bao <jc-bao@outlook.com>
2023-10-05 16:22:36 -0400

    :bug: add find usb lib

---
+++ src/crazyswarm2/crazyflie/deps/crazyflie_tools/crazyflie_cpp/cmake/Findlibusb.cmake
@@ -0,0 +1,33 @@
+# Try to find libraries and headers for libusb
+# Once done this will define
+#
+#  LIBUSB_FOUND
+#  LIBUSB_INCLUDE_DIRS
+#  LIBUSB_LIBRARIES
+
+find_path(LIBUSB_INCLUDE_DIR
+  NAMES
+  libusb.h
+  PATH_SUFFIXES
+  "include"
+  "libusb"
+  "libusb-1.0"
+)
+
+find_library(LIBUSB_LIBRARY
+  NAMES
+  usb-1.0
+  PATH_SUFFIXES
+  "lib"
+  "lib64"
+)
+
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set LIBUSB_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(libusb DEFAULT_MSG LIBUSB_LIBRARY LIBUSB_INCLUDE_DIR)
+
+mark_as_advanced(LIBUSB_INCLUDE_DIR LIBUSB_LIBRARY)
+
+set(LIBUSB_INCLUDE_DIRS ${LIBUSB_INCLUDE_DIR})
+set(LIBUSB_LIBRARIES ${LIBUSB_LIBRARY})
