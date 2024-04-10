288e845ecf1845ef6ee2e024a79cf261a451cf5f
Guanqi He <hegq@shanghaitech.edu.cn>
2024-04-10 10:54:49 +0800

    doa test trajectory

--- src/doa_experiment/doa_experiment/trajectory.py
+++ src/doa_experiment/doa_experiment/trajectory.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def generate_traj(init_pos: np.array, dt: float, mode: str = "0") -> np.ndarray:
+def generate_traj(init_pos: np.array, dt: float, mode: str) -> np.ndarray:
     """
     generate a trajectory with max_steps steps
     """
@@ -9,49 +9,40 @@ def generate_traj(init_pos: np.array, dt: float, mode: str = "0") -> np.ndarray:
     # generate take off trajectory
     target_pos = np.array([0.0, 0.0, 0.0])
     t_takeoff = 3.0
-    N_takeoff = int(t_takeoff / dt)
-    pos_takeoff = np.linspace(init_pos, target_pos, N_takeoff)
-    vel_takeoff = np.ones_like(pos_takeoff) * (target_pos - init_pos) / t_takeoff
-    acc_takeoff = np.zeros_like(pos_takeoff)
+    pos_takeoff, vel_takeoff, acc_takeoff = line(dt, t_takeoff, init_pos, target_pos)
 
     # stablize for 1.0 second
     t_stablize = 1.0
-    N_stablize = int(t_stablize / dt)
-    pos_stablize = np.ones((N_stablize, 3)) * target_pos
-    vel_stablize = np.zeros_like(pos_stablize)
-    acc_stablize = np.zeros_like(pos_stablize)
+    pos_stablize, vel_stablize, acc_stablize = line(
+        dt, t_stablize, target_pos, target_pos
+    )
 
     # generate test trajectory
     t_task = 20.0
 
-    wx = 0.4 * np.pi
-    wy = 0.2 * np.pi
-    wz = 0.0 * np.pi
-    t = np.linspace(0, t_task, int(t_task / dt))
-    # figure 8 trajectory
-    # a = 1.0  # Amplitude in x-direction
-    # b = 0.5  # Amplitude in y-direction
-    a = 1.0
-    b = 1.0
-    c = 0.0
-    x = a * np.sin(wx * t) + 0.0
-    y = b * np.sin(wy * t) + 0.0
-    z = c * np.sin(wz * t) + 0.0
-    pos_task = np.stack([x, y, z], axis=-1) + target_pos
-    vel_task = np.diff(pos_task, axis=0) / dt
-    vel_task = np.concatenate([vel_task, vel_task[-1:]], axis=0)
-    acc_task = np.diff(vel_task, axis=0) / dt
-    acc_task = np.concatenate([acc_task, acc_task[-1:]], axis=0)
+    if mode == "figure8":
+        # figure 8 trajectory
+        W = np.array([0.4, 0.2, 0.0]) * np.pi
+        A = np.array([1.0, 1.0, 0.0])
+        Phi = np.array([0.0, 0.0, 0.0])
+        pos_task, vel_task, acc_task = figure8(dt, t_task, A, W, Phi)
 
-    scale = 1.0
-    pos_task = pos_task * scale
-    vel_task = vel_task * scale
-    acc_task = acc_task * scale
+        scale = 1.0
+        pos_task = pos_task * scale
+        vel_task = vel_task * scale
+        acc_task = acc_task * scale
+
+    elif mode == "random":
+        pass
+
+    else:
+        raise ValueError("Invalid mode")
 
     # generate landing trajectory by inverse the takeoff trajectory
-    pos_landing = pos_takeoff[::-1]
-    vel_landing = -vel_takeoff[::-1]
-    acc_landing = -acc_takeoff[::-1]
+    pos_land_stablize, vel_land_stablize, acc_land_stablize = line(
+        dt, t_stablize, pos_takeoff[-1], pos_takeoff[-1]
+    )
+    pos_landing, vel_landing, acc_landing = line(dt, t_takeoff, pos_task[-1], init_pos)
 
     # concatenate all trajectories
     pos = np.concatenate(
@@ -59,7 +50,7 @@ def generate_traj(init_pos: np.array, dt: float, mode: str = "0") -> np.ndarray:
             pos_takeoff,
             pos_stablize,
             pos_task,
-            pos_stablize,
+            pos_land_stablize,
             pos_landing,
         ],
         axis=0,
@@ -69,7 +60,7 @@ def generate_traj(init_pos: np.array, dt: float, mode: str = "0") -> np.ndarray:
             vel_takeoff,
             vel_stablize,
             vel_task,
-            vel_stablize,
+            vel_land_stablize,
             vel_landing,
         ],
         axis=0,
@@ -79,7 +70,7 @@ def generate_traj(init_pos: np.array, dt: float, mode: str = "0") -> np.ndarray:
             acc_takeoff,
             acc_stablize,
             acc_task,
-            acc_stablize,
+            acc_land_stablize,
             acc_landing,
         ],
         axis=0,
@@ -102,22 +93,35 @@ def figure8(dt, T, A, W, Phi):
     return pos, vel, acc
 
 
+def line(dt, T, start, end):
+    t = np.arange(0, T, dt).reshape(-1, 1)
+    start = start.reshape(1, -1)
+    end = end.reshape(1, -1)
+    pos = start + t * (end - start) / T
+    vel = np.diff(pos, axis=0) / dt
+    vel = np.concatenate([vel, vel[-1:]], axis=0)
+    acc = np.diff(vel, axis=0) / dt
+    acc = np.concatenate([acc, acc[-1:]], axis=0)
+    return pos, vel, acc
+
 
 def test():
     import matplotlib.pyplot as plt
+
     dt = 0.02
     T = 20
-    A = np.array([1.0, 1.0, 1.0])
-    W = np.array([0.4, 0.2, 0.0]) * np.pi
-    Phi = np.array([0.0, 0.0, 0.0])
-    pos, vel, acc = figure8(dt, T, A, W, Phi)
+    start = np.array([1.0, 2.0, 3.0])
+    end = np.array([2.0, 4.0, 6.0])
+    pos, vel, acc = line(dt, T, start, end)
     x, y, z = pos[:, 0], pos[:, 1], pos[:, 2]
     fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+    ax = fig.add_subplot(111, projection="3d")
     ax.plot(x, y, z)
     fig = plt.figure()
     plt.plot(vel)
     fig = plt.figure()
     plt.plot(acc)
     plt.show()
-    
+
+
+test()
